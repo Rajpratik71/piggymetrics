c3730e77ea4df93dbf7ddf60bf6969155035c775
Said Boudjelda <bmscomp@gmail.com>
2020-07-05 23:19:34 +0200

    use junit platform

--- account-service/src/main/java/com/piggymetrics/account/client/StatisticsServiceClient.java
+++ account-service/src/main/java/com/piggymetrics/account/client/StatisticsServiceClient.java
@@ -1,6 +1,7 @@
 package com.piggymetrics.account.client;
 
 import com.piggymetrics.account.domain.Account;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
